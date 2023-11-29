b4742acc326725362441e9d3c590c5f4c67ce106
LOlsaless <hoof.jo@gmail.com>
2023-11-29 17:49:03 +0900

    fff

---
+++ 2. 연구사업(2023년)/6. 분석결과/data_analysis/PCA/PCA.R
@@ -0,0 +1,55 @@
+library(tidyverse)
+
+head(iris)
+
+view(iris)
+str(iris)
+
+iris.pca <- prcomp(iris[1:4], center = TRUE, scale. = TRUE)
+
+summary(iris.pca)
+
+plot(iris.pca, type = "l", main = "scree plot")
+
+# 차원 축소
+
+head(iris.pca$x[,1:2], 10)
+
+library(ggfortify)
+autoplot(iris.pca, data = iris, color = 'Species') # 2ckdnjsdmfh 축소된 데이터 시각화
+
+
+
+water.pca <- prcomp(PCA[6:18], center = T, scale. = T)
+
+plot(water.pca, type = "l", main = "scree plot")
+
+autoplot(water.pca, data = PCA, color = 'part')
+
+
+## 이미지 차원 축소
+
+library(jpeg)
+photo <- readJPEG('image.jpg')
+class(photo)
+
+dim(photo)
+
+# rgb 뎅터 분할 및 주성분 분석
+r <-  photo[,,1] # 행, 열, 층
+g <-  photo[,,2]
+b <-  photo[,,3]
+
+r.pca <- prcomp(r, center = F) # r 데이터 주성분분석
+g.pca <- prcomp(g, center = F) # 표준화를 하지 않는 이유는 차원 축소를 하고 나서 사진을 다시 만들어야 하기 때문이다.
+b.pca <- prcomp(b, center = F)
+
+rgb.pca <- list(r.pca, g.pca, b.pca)
+
+pc <- c(2, 10, 50, 100, 300)
+for (i in pc) {
+    pca.img <- sapply(rgb.pca, function(j) {
+        compressed.img <- j$x[, 1:i] %*% t(j$rotation[,1:i])
+    }, simplify = 'array')
+    writeJPEG(pca.img, paste("image_pca_", i, '.jpeg', sep = ""))
+}
